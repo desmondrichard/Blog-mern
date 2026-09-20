1ca1c897bd2ef912b2d8dafc7ec3cfd6fe684e30
desmondricharddoss <desmondrichard1955@gmail.com>
2026-09-21 01:07:04 +0530

    fixed header toggle again

--- frontend/src/components/GlobalHeader.jsx
+++ frontend/src/components/GlobalHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -13,6 +13,7 @@ const GlobalHeader = () => {
   const { cartCount } = useContext(cartContext);
   const { user, setUser } = useContext(authContext);
   const navigate = useNavigate();
+  const [expanded, setExpanded] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -22,15 +23,29 @@ const GlobalHeader = () => {
       console.log(error.response?.data?.message || error.message);
     } finally {
       setUser(null);
+      setExpanded(false);
       navigate("/");
     }
   };
 
+  const handleNavLinkClick = () => {
+    setExpanded(false);
+  };
   return (
     <div className="globalHeaderContainer">
-      <Navbar bg="primary" expand="lg" collapseOnSelect>
+      <Navbar
+        bg="primary"
+        expand="lg"
+        expanded={expanded}
+        onToggle={setExpanded}
+      >
         <Container className="headerWrapper">
-          <Navbar.Brand as={Link} to="/" className="headerTitle text-white">
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="headerTitle text-white"
+            onClick={handleNavLinkClick}
+          >
             My Blog
           </Navbar.Brand>
 
@@ -42,24 +57,49 @@ const GlobalHeader = () => {
           <Navbar.Collapse id="main-navbar-nav">
             <Nav className="headerPageLinksWrapper">
               {!user && (
-                <Nav.Link as={Link} to="/" className="headerLinks">
+                <Nav.Link
+                  as={Link}
+                  to="/"
+                  className="headerLinks"
+                  onClick={handleNavLinkClick}
+                >
                   Home
                 </Nav.Link>
               )}
 
               {user?.usertype === "admin" && (
-                <Nav.Link as={Link} to="/dashboard" className="headerLinks">
+                <Nav.Link
+                  as={Link}
+                  to="/dashboard"
+                  className="headerLinks"
+                  onClick={handleNavLinkClick}
+                >
                   Dashboard
                 </Nav.Link>
               )}
 
-              <Nav.Link as={Link} to="/posts" className="headerLinks">
+              <Nav.Link
+                as={Link}
+                to="/posts"
+                className="headerLinks"
+                onClick={handleNavLinkClick}
+              >
                 Posts
               </Nav.Link>
-              <Nav.Link as={Link} to="/about" className="headerLinks">
+              <Nav.Link
+                as={Link}
+                to="/about"
+                className="headerLinks"
+                onClick={handleNavLinkClick}
+              >
                 About
               </Nav.Link>
-              <Nav.Link as={Link} to="/cart" className="headerLinks">
+              <Nav.Link
+                as={Link}
+                to="/cart"
+                className="headerLinks"
+                onClick={handleNavLinkClick}
+              >
                 Cart
                 <sup>
                   <span className="cartCount">{cartCount}</span>
@@ -79,10 +119,20 @@ const GlobalHeader = () => {
                 </>
               ) : (
                 <>
-                  <Nav.Link as={Link} to="/login" className="headerLinks">
+                  <Nav.Link
+                    as={Link}
+                    to="/login"
+                    className="headerLinks"
+                    onClick={handleNavLinkClick}
+                  >
                     Login
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/register" className="headerLinks">
+                  <Nav.Link
+                    as={Link}
+                    to="/register"
+                    className="headerLinks"
+                    onClick={handleNavLinkClick}
+                  >
                     Register
                   </Nav.Link>
                 </>
